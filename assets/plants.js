b975978bbab71edfc458b6d2a0938923e10aa7ef
finnjanssens <finnjanssensfj@gmail.com>
2022-10-26 09:44:19 +0200

    Add files from Hyperdrive start repo

---
+++ assets/plants.js
@@ -0,0 +1,36 @@
+export const plants = [{
+  name: "Pannekoekenplant",
+  image: "https://boompjevoorthuis.be/wp-content/uploads/2020/09/flora_5ef36e3d2e749-scaled.jpeg",
+  water: "normaal",
+  sun: "halfschaduw"
+},
+{
+  name: "Aloë Vera",
+  image: "https://cdn.webshopapp.com/shops/271987/files/357391427/650x650x2/aloe-vera-barbadensis.jpg",
+  water: "droog",
+  sun: "halfschaduw"
+},
+{
+  name: "Araucaria",
+  image: "https://www.fleur.nl/media/contentmanager/content/Web_Araucaria%20heterophylla%202.png",
+  water: "droog",
+  sun: "halfschaduw"
+},
+{
+  name: "Gatenplant",
+  image: "https://www.ikea.com/be/nl/images/products/fejka-kunstplant-binnen-buiten-monstera__0959226_pe809439_s5.jpg?f=xl",
+  water: "normaal",
+  sun: "schaduw"
+},
+{
+  name: "Agave",
+  image: "https://cdn.webshopapp.com/shops/271987/files/323453921/agave-angustifolia.jpg",
+  water: "droog",
+  sun: "zon"
+},
+{
+  name: "Koffieplant",
+  image: "https://media.s-bol.com/qjr58L3ApkE2/550x597.jpg",
+  water: "normaal",
+  sun: "zon"
+}];
