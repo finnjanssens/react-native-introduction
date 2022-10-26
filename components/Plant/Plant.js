6ce5632c38c02784d4c45342e8b583fa31cbc80c
finnjanssens <finnjanssensfj@gmail.com>
2022-10-26 10:06:20 +0200

    Add images

--- components/Plant/Plant.js
+++ components/Plant/Plant.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 export const Plant = (props) => {
   return (
-    <View>
+    <View style={{ width: "100%" }}>
+      <Image
+        style={{ width: "80%", height: 300 }}
+        source={{ uri: props.plant.image }}
+        resizeMode={"cover"} // cover or contain its upto you view look
+      />
       <Text>{props.plant.name}</Text>
+      <Text>{props.plant.water}</Text>
+      <Text>{props.plant.sun}</Text>
     </View>
   );
 };
